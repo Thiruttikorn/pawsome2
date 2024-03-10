365da518a4f7c30dbb2978404baad9cd2b084103
Thiruttikorn <thiruttikorn.a@mail.kmutt.ac.th>
2024-03-10 21:32:48 +0700

    add data

--- js/auth.js
+++ js/auth.js
@@ -1,15 +1,5 @@
 
-var firebaseConfig = { // enter the details below
-  apiKey: "AIzaSyBtBLjODkH8iFlpPPJ31qeIIM5lDWSNgSc",
-authDomain: "pawsome-e2cd3.firebaseapp.com",
-databaseURL: "https://pawsome-e2cd3-default-rtdb.firebaseio.com",
-projectId: "pawsome-e2cd3",
-storageBucket: "pawsome-e2cd3.appspot.com",
-messagingSenderId: "584456719961",
-appId: "1:584456719961:web:db1bca363db76e90482b48",
-measurementId: "G-K6NCFMXZ40"
-};
-firebase.initializeApp(firebaseConfig);
+
 function SignUp () {
   const fname = document.getElementById('fname').value
   const lname = document.getElementById('lname').value
@@ -64,8 +54,8 @@ function SignUp () {
         alert('Registration successfully!!');
    
         if(email=="admin@admin.com"){
-          setHrefManage('admin-manage.html');
-          window.location.href = 'admin-manage.html';
+          setHrefManage('booking-manage.html');
+          window.location.href = 'booking-manage.html';
         }else{
           setHrefManage('user-manage.html');
           window.location.href = 'user-manage.html';
@@ -105,164 +95,53 @@ function SignUp () {
   // }
 }
 
-function SignIn () {
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
-  if (email == '') {
-    alert('กรุณาระบุอีเมล')
-    return false
-  } else if (password == '') {
-    alert('กรุณาระบุรหัสผ่าน')
-    return false
-  }
-  firebase
-    .auth().signInWithEmailAndPassword(email,password)
-    .then(userCredential => {
-      // Signed in
-      const user = userCredential.user
-      // console.log(user)
-      alert(user.email + ' Login successfully!!!')
-
-      document.getElementById('signOut').style.display = 'block';
-      if(email=="admin@admin.com"){
-        setHrefManage('admin-manage.html');
-          window.location.href = 'admin-manage.html';
-        }else{
-          setHrefManage('user-manage.html');
-          window.location.href = 'user-manage.html';
-        }
-    })
-    .catch(error => {
-      const errorCode = error.code
-      const errorMessage = error.message
-      console.log(errorMessage)
-      alert("ไม่สามารถลงชื่อเข้าใช้งานได้ กรุณาสมัครสมาชิก");
-      document.getElementById("nav-register-tab").click();
-    })
-  // var postListRef = firebase.database().ref('users');
-  // var chk = "";
-  // postListRef.on('value', function (snapshot) {
-  //   snapshot.forEach(function (childSnapshot) {
-  //     var childData = childSnapshot.val();
-  //     if (childData.email == email && childData.password == password) {
-  //       chk = true;
-  //       return false;
-  //     } else {
-  //       chk = false;
-  //       return false;
-  //     }
-  //   });
-  // });
-  // if (chk === true) {
-  //   firebase.auth().createUserWithEmailAndPassword(email, password)
-  //     .then(() => {
-  //       alert("ลงชื่อเข้าใช้งานสำเร็จ")
-  //       firebase.auth().onAuthStateChanged(user => {
-  //         if (user) {
-  //     document.getElementById("signOut").style.display = "inline-block";
-  //     user.getIdToken().then(function(accessToken) {
-  //       document.getElementById("usernametext").innerHTML = user.email;
-  //       document.getElementById("usernametext2").innerHTML = user.email
-
-  //                     });
-
-  //           window.location.href = "index.html";
-  //         }
-
-  //       });
-  //       // Signed in
-  //       //  user.sendEmailVerification().then(function(){
-  //       //     alert("ส่งอีเมลยืนยันผู้ใช้ไปที่"+email)
-  //       //  }).catch(function(error){
-  //       //       alert(error.message)
-  //       //  });
-
-  //     })
-  //     .catch((error) => {
-  //       alert("ลงชื่อเข้าใช้งานไม่สำเร็จ กรุณากรอกข้อมูลใหม่อีกครั้ง");
-  //       // ..
-  //     });
-  // } else {
-  //   alert("ข้อมูลไม่ถูกต้อง กรุณากรอกข้อมูลใหม่อีกครั้ง");
-
-  // }
-}
 
 window.addEventListener('load', function () {
   initApp()
-})
-function initApp () {
-  // const userss = firebase.auth().currentUser
+});
 
+function initApp () {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      console.log(user)
-      document.getElementById('signOut').style.display = 'inline-block'
-      user.getIdToken().then(function (accessToken) {
+
+      document.getElementById('signOut').style.display = 'inline-block';
+
+      user.getIdToken().then(function () {
         if(user.email=="admin@admin.com"){
-          setHrefManage('admin-manage.html');
+          setHrefManage('booking-manage.html');
         }else{
           setHrefManage('user-manage.html');
+
         }
         document.getElementById('usernametext').innerHTML = user.email;
         document.getElementById('usernametext2').innerHTML = user.email;
+        hideBooking("block");
       })
     } else {
       document.getElementById('usernametext').innerHTML = '';
       document.getElementById('usernametext2').innerHTML = '';
+      hideBooking("none");
     }
   })
 }
 
-document.getElementById('signOut').addEventListener('click', function (event) {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
-      //     const user = firebase.auth().currentUser;
-      // user.delete().then(() => {
-      //   location.reload()
-      // }).catch((error) => {
-      //     // An error occurred
-      //     // ...
-      // });
+// document.getElementById('signOut').addEventListener('click', function (event) {
+//   firebase
+//     .auth()
+//     .signOut()
+//     .then(() => {
+//       location.replace("login.html")
+//     })
+//     .catch(error => {
+//       alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
+//     })
+// })
 
-      location.reload()
-    })
-    .catch(error => {
-      alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-    })
-})
 
-document.querySelectorAll(".booking").forEach(item => {
-  item.addEventListener("click", () => {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        if(user.email=="admin@admin.com"){
-          setHrefManage('admin-manage.html');
-          window.location.href = 'admin-manage.html';
-        }else{
-          setHrefManage('user-manage.html');
-          window.location.href = 'user-manage.html';
-        }
-        
-        // document.getElementById('signOut').style.display = 'inline-block'
-        // user.getIdToken().then(function (accessToken) {
-        //   document.getElementById('usernametext').innerHTML = user.email
-        //   document.getElementById('usernametext2').innerHTML = user.email
-        // })
-      } else {
-        alert('กรุณาลงชื่อเข้าใช้ระบบ');
-        window.location.href = 'login.html';
-      }
-    })
-  });
-});
 
 function setHrefManage(page){
   document.querySelectorAll(".bookicon").forEach(item => {
     item.href = page;
-    console.log(page)
   });
   
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+var firebaseConfig = { // enter the details below
+  apiKey: "AIzaSyBtBLjODkH8iFlpPPJ31qeIIM5lDWSNgSc",
+authDomain: "pawsome-e2cd3.firebaseapp.com",
+databaseURL: "https://pawsome-e2cd3-default-rtdb.firebaseio.com",
+projectId: "pawsome-e2cd3",
+storageBucket: "pawsome-e2cd3.appspot.com",
+messagingSenderId: "584456719961",
+appId: "1:584456719961:web:db1bca363db76e90482b48",
+measurementId: "G-K6NCFMXZ40"
+};
+firebase.initializeApp(firebaseConfig);
 (function($) {
   
   "use strict";
@@ -184,17 +195,51 @@
   }); // End of a document
 
 })(jQuery);
-// var admin = require("firebase-admin");
 
-//   var serviceAccount = require("path/to/serviceAccountKey.json");
-  
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: "https://pawsome-e2cd3-default-rtdb.firebaseio.com"
-//   });
+function hideBooking(style){
+  document.querySelectorAll(".bookicon").forEach(item => {
+    item.style.display = style;
+  });
+}
+document.querySelectorAll(".booking").forEach(item => {
+  item.addEventListener("click", () => {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        if(user.email=="admin@admin.com"){
+          setHrefManage('booking-manage.html');
+          window.location.href = 'booking-manage.html';
+        }else{
+          setHrefManage('user-manage.html');
+          window.location.href = 'user-manage.html';
+        }
+        
+      } else {
+        alert('กรุณาลงชื่อเข้าใช้ระบบ');
+        window.location.href = 'login.html';
+      }
+    })
+  });
+});
 
-// function reload() {
-//   google.script.run.withSuccessHandler(function(url) {
-//       window.open(url, '_top');
-//   }).webAppUrl();
-// }
+document.querySelectorAll(".signout").forEach(item => {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      item.style.display = "block";
+      item.addEventListener("click", () => {
+        firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          location.replace("login.html")
+        })
+        .catch(error => {
+          alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
+        })
+      });
+    } else {
+      item.style.display = "none";
+    }
+  })
+
+
+});
