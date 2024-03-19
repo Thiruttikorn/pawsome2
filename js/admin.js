0ffcff37df161f298b1e70549bb56bb0f52763f7
Thiruttikorn <thiruttikorn.a@mail.kmutt.ac.th>
2024-03-19 23:28:08 +0700

    create function update

--- js/admin.js
+++ js/admin.js
@@ -1,90 +1,66 @@
-
 window.addEventListener('load', function () {
-  selectAnimal();
-
+  selectAnimal()
 })
+// ========= Animal Type ==============================
 document
   .getElementById('v-pills-pets-tab')
   .addEventListener('click', function (event) {
     selectAnimal()
   })
-document
-  .getElementById('v-pills-service-tab')
-  .addEventListener('click', function (event) {
-    selectService()
-  })
+  function selectAnimal () {
+    var postListRef = firebase.database().ref('animal_type')
+    var html = ''
+    document.getElementById('updateanimal').style.display = 'none'
+    document.getElementById('saveanimal').style.display = 'inline-block'
 
- function selectAnimal () {
-  var postListRef = firebase.database().ref('animal_type')
-  var html = ''
-  document.getElementById('table-animaltype').innerHTML = ''
-  postListRef.on(
-    'value',
-     function (snapshot) {
+    document.getElementById('animaltype').value = ''
+
+    document.getElementById('animalid').value = ''
+
+    document.getElementById('animalcode').value = ''
+
+    document.getElementById('animalactive').value = ''
+    document.getElementById('animalkey').value = ''
+
+    document.getElementById('table-animaltype').innerHTML = ''
+    postListRef.on('value', function (snapshot) {
       snapshot.forEach(function (childSnapshot) {
         const data = childSnapshot.val()
         html += `<tr>
-    <th scope="row">${data.ANM_ID}</th>
-    <td>${data.ANM_Name}</td>
-    <td>${data.ANM_Code}</td>
-    <td>
-    ${
-      data.ANM_Active == '1'
-        ? `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: greenyellow;"></iconify-icon>
-    ปกติ`
-        : `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: red;"></iconify-icon>
-    ไม่ใช้งาน`
-    }</td>
-   <td> <button class="w-auto btn btn-secondary btn-sm p-1 delanimal"
-            type="button" value="${childSnapshot.key}"><iconify-icon icon="mdi:trash-can-outline"></iconify-icon> ไม่ใช้งาน
-      
-        </button></td> 
-</tr>`
+      <th scope="row">${data.ANM_ID}</th>
+      <td>${data.ANM_Name}</td>
+      <td>${data.ANM_Code}</td>
+      <td>
+      ${
+        data.ANM_Active == '1'
+          ? `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: greenyellow;"></iconify-icon>
+      ปกติ`
+          : `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: red;"></iconify-icon>
+      ไม่ใช้งาน`
+      }</td>
+    <td> <button class="w-auto btn btn-danger btn-sm p-1 delanimal"
+              type="button" value="${
+                childSnapshot.key
+              }"><iconify-icon icon="mdi:trash-can-outline"></iconify-icon> ไม่ใช้งาน
+        
+          </button>
+          <button class="w-auto btn btn-warning btn-sm p-1 updateanimal"
+              type="button" value="${
+                childSnapshot.key
+              }"><iconify-icon icon="mdi:edit-outline"></iconify-icon> แก้ไข
+        
+          </button></td> 
+          
+  </tr>`
       })
-      document.getElementById('table-animaltype').innerHTML = html;
-      delanimal();
+      document.getElementById('table-animaltype').style.display =
+        'table-row-group'
 
-    }
-  )
- 
-}
-
-
- function selectService () {
-  var postListRef = firebase.database().ref('service_type')
-  var html = ''
-  document.getElementById('table-servicetype').innerHTML = ''
-  postListRef.on(
-    'value',
-     function (snapshot) {
-      snapshot.forEach(function (childSnapshot) {
-        const data = childSnapshot.val()
-
-        html += `<tr>
-    <th scope="row">${data.SV_ID}</th>
-    <td>${data.SV_Name}</td>
-    <td>${data.SV_Code}</td>
-    <td>
-    ${
-      data.SV_Active == '1'
-        ? `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: greenyellow;"></iconify-icon>
-    ปกติ`
-        : `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: red;"></iconify-icon>
-    ไม่ใช้งาน`
-    }</td>
-    <td> <button class="w-auto btn btn-secondary btn-sm p-1 delservice"
-    type="button" value="${childSnapshot.key}"><iconify-icon icon="mdi:trash-can-outline"></iconify-icon> ไม่ใช้งาน
-
-</button></td> 
-</tr>`
-      })
-      document.getElementById('table-servicetype').innerHTML = html;
-      delservice();
-    }
-  )
-}
-
-// table-animaltype
+      document.getElementById('table-animaltype').innerHTML = html
+      delanimal()
+      editanimal()
+    })
+  }
 document
   .getElementById('saveanimal')
   .addEventListener('click', function (event) {
@@ -127,7 +103,134 @@ document
       )
     }
   })
+  function delanimal () {
+    document.querySelectorAll('.delanimal').forEach(item => {
+      item.addEventListener('click', function (event) {
+        document.getElementById('table-animaltype').innerHTML = ''
+        firebase
+          .database()
+          .ref('animal_type')
+          .child(event.target.value)
+          .update({ ANM_Active: '0' })
+          .then(() => {
+            document.getElementById('table-animaltype').innerHTML = ''
+            alert('อัพเดทสถานะสำเร็จ')
+            selectAnimal()
+          })
+          .catch(error => {
+            alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
+          })
+       
+      })
+    })
+  }
+  function editanimal () {
+    document.querySelectorAll('.updateanimal').forEach(item => {
+      item.addEventListener('click', function (event) {
+        document.getElementById('updateanimal').style.display = 'inline-block'
+        document.getElementById('saveanimal').style.display = 'none'
+        document.getElementById('animalkey').value = event.target.value
+        firebase
+          .database()
+          .ref('animal_type')
+          .child(event.target.value)
+          .on('child_added', snapshot => {
+            const newPost = snapshot.val()
+            if (snapshot.key == 'ANM_Name') {
+              document.getElementById('animaltype').value = newPost
+            } else if (snapshot.key == 'ANM_ID') {
+              document.getElementById('animalid').value = newPost
+            } else if (snapshot.key == 'ANM_Code') {
+              document.getElementById('animalcode').value = newPost
+            } else if (snapshot.key == 'ANM_Active') {
+              document.getElementById('animalactive').value = newPost
+            }
+          })
+        document
+          .getElementById('updateanimal')
+          .addEventListener('click', function (event) {
+            document.getElementById('table-animaltype').innerHTML = ''
+            updateanimal()
+          })
+       
+      })
+    })
+  }
+  function updateanimal () {
+    const animaltype = document.getElementById('animaltype').value
+    const animalid = document.getElementById('animalid').value
+    const animalactive = document.getElementById('animalactive').value
+    const animalcode = document.getElementById('animalcode').value
+    const animalkey = document.getElementById('animalkey').value
+    document.getElementById('table-animaltype').style.display = 'none'
+    if (animaltype == '') {
+      alert('กรุณาระบุประเภทสัตว์')
+      return false
+    } else {
+      document.getElementById('table-animaltype').style.display = 'none'
+      firebase
+        .database()
+        .ref('animal_type')
+        .child(animalkey)
+        .update({
+          ANM_ID: animalid,
+          ANM_Name: animaltype,
+          ANM_Code: animalcode,
+          ANM_Active: animalactive
+        })
+        .then(() => {
+          document.getElementById('table-animaltype').style.display = 'none'
+          alert('อัพเดทข้อมูลสำเร็จ')
+  
+          selectAnimal()
+        })
+        .catch(error => {
+          alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
+        })
+    }
+  
+  }
+// ========= End Animal Type ==============================
 
+// ========= Service Type ==============================
+document
+  .getElementById('v-pills-service-tab')
+  .addEventListener('click', function (event) {
+    selectService()
+  })
+function selectService () {
+  var postListRef = firebase.database().ref('service_type')
+  var html = ''
+
+  document.getElementById('table-servicetype').innerHTML = ''
+  postListRef.on('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      const data = childSnapshot.val()
+
+      html += `<tr>
+    <th scope="row">${data.SV_ID}</th>
+    <td>${data.SV_Name}</td>
+    <td>${data.SV_Code}</td>
+    <td>
+    ${
+      data.SV_Active == '1'
+        ? `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: greenyellow;"></iconify-icon>
+    ปกติ`
+        : `<iconify-icon icon="ic:baseline-fiber-manual-record" style="color: red;"></iconify-icon>
+    ไม่ใช้งาน`
+    }</td>
+    <td> <button class="w-auto btn btn-secondary btn-sm p-1 delservice"
+    type="button" value="${
+      childSnapshot.key
+    }"><iconify-icon icon="mdi:trash-can-outline"></iconify-icon> ไม่ใช้งาน
+
+</button></td> 
+</tr>`
+    })
+    document.getElementById('table-servicetype').innerHTML = html
+    delservice()
+  })
+}
 document
   .getElementById('saveservice')
   .addEventListener('click', function (event) {
@@ -171,81 +274,27 @@ document
       )
     }
   })
-
-
-
-
-  
-function delanimal(){
-  console.log(document.querySelectorAll(".delanimal"))
-  document.querySelectorAll(".delanimal").forEach(item => {
-    item.addEventListener("click", function (event) {
-      document.getElementById('table-animaltype').innerHTML = '';
-      firebase.database().ref("animal_type").child(event.target.value).update({'ANM_Active': "0"}).then(() => {
-        document.getElementById('table-animaltype').innerHTML = '';
-          alert('อัพเดทสถานะสำเร็จ');
-          selectAnimal()
-              })
-              .catch(error => {
-                alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-              })
-      // firebase.database().ref("animal_type/"+event.target.value).update({
-      //   ANM_Active : 0
-      // }).then(() => {
-      
-      //   alert('อัพเดทสถานะสำเร็จ');
-      //   selectAnimal()
-      //       })
-      //       .catch(error => {
-      //         alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-      //       })
-      // firebase.database().ref("animal_type/"+event.target.value).remove().then(() => {
-      
-      //   alert('อัพเดทสถานะสำเร็จ');
-      //   selectAnimal()
-      //       })
-      //       .catch(error => {
-      //         alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-      //       })
-    });
-
-
-
-});
-}
-function delservice(){
-  document.querySelectorAll(".delservice").forEach(item => {
-    item.addEventListener("click", function (event) {
-      document.getElementById('table-servicetype').innerHTML = '';
-      firebase.database().ref("service_type").child(event.target.value).update({'SV_Active': "0"}).then(() => {
-        document.getElementById('table-servicetype').innerHTML = '';
-          alert('อัพเดทสถานะสำเร็จ');
+function delservice () {
+  document.querySelectorAll('.delservice').forEach(item => {
+    item.addEventListener('click', function (event) {
+      document.getElementById('table-servicetype').innerHTML = ''
+      firebase
+        .database()
+        .ref('service_type')
+        .child(event.target.value)
+        .update({ SV_Active: '0' })
+        .then(() => {
+          document.getElementById('table-servicetype').innerHTML = ''
+          alert('อัพเดทสถานะสำเร็จ')
           selectService()
-              })
-              .catch(error => {
-                alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-              })
-      // firebase.database().ref("animal_type/"+event.target.value).update({
-      //   ANM_Active : 0
-      // }).then(() => {
-      
-      //   alert('อัพเดทสถานะสำเร็จ');
-      //   selectAnimal()
-      //       })
-      //       .catch(error => {
-      //         alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-      //       })
-      // firebase.database().ref("animal_type/"+event.target.value).remove().then(() => {
-      
-      //   alert('อัพเดทสถานะสำเร็จ');
-      //   selectAnimal()
-      //       })
-      //       .catch(error => {
-      //         alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-      //       })
-    });
-
-
-
-});
+        })
+        .catch(error => {
+          alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
+        })
+    })
+  })
 }
+// ========= And Service Type ==============================
+
+
+
--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,10 @@
-
-
 function SignUp () {
   const fname = document.getElementById('fname').value
   const lname = document.getElementById('lname').value
   const email = document.getElementById('email-regis').value
   const password = document.getElementById('password-regis').value
   const repassword = document.getElementById('repassword-regis').value
-  var user = firebase.auth().currentUser
+  // var user = firebase.auth().currentUser
   var auth = firebase.auth()
   if (fname == '') {
     alert('กรุณาระบุชื่อผู้ใช้')
@@ -27,38 +25,41 @@ function SignUp () {
     alert('กรุณาระบุรหัสผ่านให้ตรงกัน')
     return false
   } else {
-    var postListRef = firebase.database().ref('users');
-    var newPostRef = postListRef.push();
+    var postListRef = firebase.database().ref('users')
+    var newPostRef = postListRef.push()
 
     auth
       .createUserWithEmailAndPassword(email, password)
       .then(userCredential => {
-        newPostRef.set({
-          name: fname+" "+lname,
-          email: email,
-          password: password
-        }, (error) => {
-          if (error) {
-            console.log("เพิ่มบัญชีผู้ใช้ไม่สำเร็จ กรุณาติดต่อเจ้าหน้าที่");
-            return false
-            // The write failed...
-          } else {
-            console.log("เพิ่มบัญชีผู้ใช้สำเร็จ");
-            // location.reload()
-            // Data saved successfully!
-            return false
+        newPostRef.set(
+          {
+            name: fname + ' ' + lname,
+            email: email,
+            password: password
+          },
+          error => {
+            if (error) {
+              console.log('เพิ่มบัญชีผู้ใช้ไม่สำเร็จ กรุณาติดต่อเจ้าหน้าที่')
+              return false
+              // The write failed...
+            } else {
+              console.log('เพิ่มบัญชีผู้ใช้สำเร็จ')
+              // location.reload()
+              // Data saved successfully!
+              return false
+            }
           }
-        });
-        const user = userCredential.user;
-        console.log(user);
-        alert('Registration successfully!!');
-   
-        if(email=="admin@admin.com"){
-          setHrefManage('booking-manage.html');
-          window.location.href = 'booking-manage.html';
-        }else{
-          setHrefManage('user-manage.html');
-          window.location.href = 'user-manage.html';
+        )
+        const user = userCredential.user
+        console.log(user)
+        alert('Registration successfully!!')
+
+        if (email == 'admin@admin.com') {
+          setHrefManage('booking-manage.html')
+          window.location.href = 'booking-manage.html'
+        } else {
+          setHrefManage('user-manage.html')
+          window.location.href = 'user-manage.html'
         }
       })
       .catch(error => {
@@ -68,7 +69,6 @@ function SignUp () {
         console.log(errorMessage)
         alert('เกิดข้อผิดพลาด กรุณาสมัครสมาชิกใหม่อีกครั้ง')
       })
-
   }
 
   // if (chk === false) {
@@ -95,53 +95,35 @@ function SignUp () {
   // }
 }
 
-
 window.addEventListener('load', function () {
   initApp()
-});
+})
 
 function initApp () {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-
-      document.getElementById('signOut').style.display = 'inline-block';
+      document.getElementById('signOut').style.display = 'inline-block'
 
       user.getIdToken().then(function () {
-        if(user.email=="admin@admin.com"){
-          setHrefManage('booking-manage.html');
-        }else{
-          setHrefManage('user-manage.html');
-
+        if (user.email == 'admin@admin.com') {
+          setHrefManage('booking-manage.html')
+        } else {
+          setHrefManage('user-manage.html')
         }
-        document.getElementById('usernametext').innerHTML = user.email;
-        document.getElementById('usernametext2').innerHTML = user.email;
-        hideBooking("block");
+        document.getElementById('usernametext').innerHTML = user.email
+        document.getElementById('usernametext2').innerHTML = user.email
+        hideBooking('block')
       })
     } else {
-      document.getElementById('usernametext').innerHTML = '';
-      document.getElementById('usernametext2').innerHTML = '';
-      hideBooking("none");
+      document.getElementById('usernametext').innerHTML = ''
+      document.getElementById('usernametext2').innerHTML = ''
+      hideBooking('none')
     }
   })
 }
 
-// document.getElementById('signOut').addEventListener('click', function (event) {
-//   firebase
-//     .auth()
-//     .signOut()
-//     .then(() => {
-//       location.replace("login.html")
-//     })
-//     .catch(error => {
-//       alert('เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่')
-//     })
-// })
-
-
-
-function setHrefManage(page){
-  document.querySelectorAll(".bookicon").forEach(item => {
-    item.href = page;
-  });
-  
+function setHrefManage (page) {
+  document.querySelectorAll('.bookicon').forEach(item => {
+    item.href = page
+  })
 }
